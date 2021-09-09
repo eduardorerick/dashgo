627fbbd620631b9052bfa8e8b45bab055cf86bc4
Eduardo Rerick <eduardorerick@gmail.com>
2021-09-09 16:19:17 -0300

    visual do componente de  paginação criado

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Siderbar } from '../../components/Sidebar';
 
 
@@ -107,6 +108,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
